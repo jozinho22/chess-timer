e2412669e405dd9604b06789f9e0dd1d77fe11ec
jozinho <josselin.douineau.1987@gmail.com>
2023-01-20 01:32:12 +0100

    replace by hydrateRoot react v18

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM, { hydrateRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.hydrate(
+const container = document.getElementById('root');
+hydrateRoot(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    container
 );
 
 // If you want to start measuring performance in your app, pass a function
